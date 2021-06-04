d67047c00b2992a94f7d41b12639eb0c8273fffb
Tiago Oliveira <tiagolivesan@outlook.com>
2021-06-04 12:29:00 -0300

    new-portfólio

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
+import { Header } from '../components/Header'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-		 <Component {...pageProps} />
+      <Header />
+		    <Component {...pageProps} />
 		</ChakraProvider>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import { Flex } from "@chakra-ui/react";
-import { Header } from "../components/header";
+import { Header } from "../components/Header";
+
 
 
 
 export default function Home() {
   return (
-    <Flex direction="column" h="100vh">
-        <Header />
+    <Flex direction="column">
+       
     </Flex>
   )
 }
