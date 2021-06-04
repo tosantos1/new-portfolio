8335fea86e242874709c43d39bdc392035932bbc
Tiago Oliveira <tiagolivesan@outlook.com>
2021-06-04 13:31:56 -0300

    new-portfólio

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,40 @@
-import { Flex } from "@chakra-ui/react";
-import { Header } from "../components/Header";
+import { Avatar, Button, Center, Flex, Heading, HStack, IconButton, VStack } from "@chakra-ui/react";
 
+import { FiGithub, FiLinkedin } from 'react-icons/fi'
 
 
 
 export default function Home() {
   return (
-    <Flex direction="column">
-       
+    <Flex direction="column" maxW="1200px" margin="0 auto">
+      <Center>
+        <VStack mt={4}>
+          <Heading size="lg" p={2}>Bem vindos 👊</Heading>
+          <Heading size="xl" textAlign="center" maxW="650px">Sou estudante de engenharia da computação, tenho pratica em Front End e atualmente estudando Back End.</Heading>
+          <HStack p={8}>
+            <Avatar boxSize="125px" mr={4} name="Tiago Oliveira" src="https://github.com/tosantos1.png" />
+            <VStack align="flex-start">
+              <p>Tiago Oliveira</p>
+              <p>tiagolivesan@outlook.com</p>
+              <HStack>
+            <IconButton
+              bg="#333"
+              aria-label="Github"
+              _hover={{opacity: 0.8}}
+              icon={<FiGithub />}
+            />
+            <IconButton
+              colorScheme="linkedin"
+              aria-label="Linkedin"
+              _hover={{opacity: 0.8}}
+              icon={<FiLinkedin />}
+            />
+          </HStack>
+            </VStack>
+          </HStack>
+          
+        </VStack>
+      </Center>
     </Flex>
   )
 }
